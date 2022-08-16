2595d1e2784b90e98375d1d971e9bfa78ba74182
Tran Xuan Quang <quangtxhe153654@fpt.edu.vn>
2022-08-16 16:21:20 +0700

    ADO demo

---
+++ Winform_ADO_NET/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Winform_ADO_NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.unitPriceBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.imgBox = new System.Windows.Forms.TextBox();
+            this.unitInStockBox = new System.Windows.Forms.NumericUpDown();
+            this.categoryBox = new System.Windows.Forms.ComboBox();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.dataProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.unitInStockBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(266, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRODUCT MANAGEMENT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ProductID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "UnitInStock";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "UnitPrice";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(485, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ProductName";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(485, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Image";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(485, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Category";
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(145, 114);
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(143, 23);
+            this.idBox.TabIndex = 7;
+            // 
+            // unitPriceBox
+            // 
+            this.unitPriceBox.Location = new System.Drawing.Point(145, 167);
+            this.unitPriceBox.Name = "unitPriceBox";
+            this.unitPriceBox.Size = new System.Drawing.Size(143, 23);
+            this.unitPriceBox.TabIndex = 8;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(596, 114);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(143, 23);
+            this.nameBox.TabIndex = 9;
+            this.nameBox.Enter += new System.EventHandler(this.nameBox_Enter);
+            this.nameBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nameBox_KeyDown);
+            // 
+            // imgBox
+            // 
+            this.imgBox.Location = new System.Drawing.Point(596, 167);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(143, 23);
+            this.imgBox.TabIndex = 10;
+            // 
+            // unitInStockBox
+            // 
+            this.unitInStockBox.Location = new System.Drawing.Point(153, 236);
+            this.unitInStockBox.Name = "unitInStockBox";
+            this.unitInStockBox.Size = new System.Drawing.Size(143, 23);
+            this.unitInStockBox.TabIndex = 11;
+            // 
+            // categoryBox
+            // 
+            this.categoryBox.FormattingEnabled = true;
+            this.categoryBox.Location = new System.Drawing.Point(596, 236);
+            this.categoryBox.Name = "categoryBox";
+            this.categoryBox.Size = new System.Drawing.Size(143, 23);
+            this.categoryBox.TabIndex = 12;
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(187, 314);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(75, 23);
+            this.insertButton.TabIndex = 13;
+            this.insertButton.Text = "INSERT";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(376, 314);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 14;
+            this.updateButton.Text = "UPDATE";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(566, 314);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 15;
+            this.deleteButton.Text = "DELETE";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // dataProduct
+            // 
+            this.dataProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataProduct.Location = new System.Drawing.Point(79, 376);
+            this.dataProduct.Name = "dataProduct";
+            this.dataProduct.RowTemplate.Height = 25;
+            this.dataProduct.Size = new System.Drawing.Size(660, 150);
+            this.dataProduct.TabIndex = 16;
+            this.dataProduct.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataProduct_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 555);
+            this.Controls.Add(this.dataProduct);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.categoryBox);
+            this.Controls.Add(this.unitInStockBox);
+            this.Controls.Add(this.imgBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.unitPriceBox);
+            this.Controls.Add(this.idBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.unitInStockBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.TextBox unitPriceBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox imgBox;
+        private System.Windows.Forms.NumericUpDown unitInStockBox;
+        private System.Windows.Forms.ComboBox categoryBox;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.DataGridView dataProduct;
+    }
+}
---
+++ Winform_ADO_NET/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_ADO_NET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        DataTable dt = new DataTable();
+        //load data len datagridview va combox category
+        private void loadData()
+        {
+            string strSelect = "select ProductID as 'Ma san pham', ProductName, UnitPrice, UnitsInStock, Image, c.CategoryName from Products p, Categories c where c.CategoryID = p.CategoryID";
+            
+            dt = new DataProvider().executeQuery(strSelect);
+            //Customize data
+            //DataTable dt2 = new DataTable();
+            //dt2.Columns.Add("ID", typeof(string));
+            //dt2.Columns.Add("Name", typeof(string));
+            //foreach (DataRow item in dt.Rows)
+            //{
+            //    string code = "P " + item.ItemArray[0].ToString();
+            //    string name = item.ItemArray[1].ToString();
+            //    dt2.Rows.Add(new object[] { code, name });
+            //}
+
+
+            dataProduct.DataSource = dt;
+            dt = new DataProvider().executeQuery("Select * from Categories");
+            categoryBox.DataSource = dt;
+            categoryBox.DisplayMember = "CategoryName";
+            categoryBox.ValueMember = "CategoryId";
+        }
+
+        private void dataProduct_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            idBox.ReadOnly = true;
+            idBox.Text = dataProduct.Rows[e.RowIndex].Cells[0].FormattedValue.ToString();
+            nameBox.Text = dataProduct.Rows[e.RowIndex].Cells["ProductName"].FormattedValue.ToString();
+            unitPriceBox.Text = dataProduct.Rows[e.RowIndex].Cells["UnitPrice"].FormattedValue.ToString();
+            unitInStockBox.Value = Convert.ToDecimal(dataProduct.Rows[e.RowIndex].Cells["UnitsInStock"].FormattedValue.ToString());
+            categoryBox.Text = dataProduct.Rows[e.RowIndex].Cells["CategoryName"].FormattedValue.ToString();
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            string insert = "insert into Products(ProductName,UnitPrice,UnitsInStock,Image,CategoryID) values ("
+                + "'" + nameBox.Text + "',"
+                + "'" + unitPriceBox.Text + "',"
+                + Convert.ToInt32(unitInStockBox.Text)+","
+                + "'" + imgBox.Text + "',"
+                + categoryBox.SelectedValue
+                + ")";
+            if (new DataProvider().executeNonQuery(insert))
+            {
+                MessageBox.Show("Inserted");
+                loadData();
+            }
+            else
+            {
+                MessageBox.Show("Insert sai");
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            string update = "update Products set"
+                    + " ProductName = '" + nameBox.Text + "',"
+                    + " UnitPrice = '" + unitPriceBox.Text + "',"
+                    + " UnitsInStock =" + Convert.ToInt32(unitInStockBox.Text) + ","
+                    + " Image = '" + imgBox.Text + "',"
+                    + " CategoryID =" + categoryBox.SelectedValue + " where ProductID =" + Convert.ToInt32(idBox.Text);
+            if (dataProduct.SelectedCells != null)
+            {
+                if (new DataProvider().executeNonQuery(update))
+                {
+                    MessageBox.Show("Updated");
+                    loadData();
+                }
+                else
+                {
+                    MessageBox.Show("Insert sai");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui long chon de update");
+            }          
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (dataProduct.SelectedCells != null)
+            {
+                string delete = "delete from Products where ProductID ="+idBox.Text;
+                if (new DataProvider().executeNonQuery(delete))
+                {
+                    MessageBox.Show("Deleted");
+                    loadData();
+                }
+                else
+                {
+                    MessageBox.Show("Deleted sai");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui long chon de delete");
+            }
+        }
+
+        private void nameBox_Enter(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void nameBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (string.IsNullOrEmpty(nameBox.Text))
+                {
+                    string name = nameBox.Text.Trim();
+                    string select = "select * from Products where ProductName = '" + name + "'";
+                    dt = new DataProvider().executeQuery(select);
+                    dataProduct.DataSource = dt;
+                    dataProduct.Refresh();
+                }
+                else
+                {
+                    loadData();
+                }
+            }
+        }
+    }
+}
